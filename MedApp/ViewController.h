a8442bed57bfee76818272d51080b7d05c5c7ae3
Beltik <delaviar@gmail.com>
2016-01-22 18:46:54 +0300

    Initial Commit

---
+++ MedApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MedApp
+//
+//  Created by Necrosoft on 22/01/16.
+//  Copyright © 2016 Necrosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
