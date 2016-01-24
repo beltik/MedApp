e6dc8e475949b300b31543a061d869465f3eef08
Beltik <delaviar@gmail.com>
2016-01-24 21:34:51 +0300

    Detail Controller

---
+++ MedApp/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  MedApp
+//
+//  Created by Necrosoft on 24/01/16.
+//  Copyright © 2016 Necrosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ MedApp/DetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.m
+//  MedApp
+//
+//  Created by Necrosoft on 24/01/16.
+//  Copyright © 2016 Necrosoft. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@implementation DetailViewController
+
+@end
