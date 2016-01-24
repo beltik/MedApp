f08f1af877c0a2fab771d05eeb186cc96d302132
Beltik <delaviar@gmail.com>
2016-01-25 00:50:28 +0300

    Minor bug fixes

---
+++ MedApp/DetailNewsParseer.h
@@ -0,0 +1,13 @@
+//
+//  DetailNewsParseer.h
+//  MedApp
+//
+//  Created by Necrosoft on 25/01/16.
+//  Copyright © 2016 Necrosoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DetailNewsParseer : NSObject
+
+@end
---
+++ MedApp/DetailNewsParseer.m
@@ -0,0 +1,13 @@
+//
+//  DetailNewsParseer.m
+//  MedApp
+//
+//  Created by Necrosoft on 25/01/16.
+//  Copyright © 2016 Necrosoft. All rights reserved.
+//
+
+#import "DetailNewsParseer.h"
+
+@implementation DetailNewsParseer
+
+@end
